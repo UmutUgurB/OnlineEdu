5b9dabf03ea695e3d733c46f43c95d149a4fe817
UmutUgurB <umutugurb@gmail.com>
2025-05-05 14:24:02 +0300

    social media controller

---
+++ OnlineEdu.DTOLayer/DTOs/SocialMediaDtos/UpdateSocialMediaDto.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineEdu.DTOLayer.DTOs.SocialMediaDtos
+{
+    public class UpdateSocialMediaDto
+    {
+        public int SocialMediaID { get; set; }
+        public string Icon { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ OnlineEdu.WebApi/Controllers/SocialMediasController.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.BusinessLayer.Abstract;
+using OnlineEdu.EntityLayer.Entities;
+
+namespace OnlineEdu.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocialMediasController(IGenericService<SocialMedia> genericService,IMapper mapper) : ControllerBase
+    {
+    }
+}
